66dae2ed211d4bdf7a1b8031ba7b004f5d4d8209
boldboxer <ebesteinpr@gmail.com>
2023-02-20 12:13:35 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- prints alphabets in lower case
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+	putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
+

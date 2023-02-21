6f4767bd6256423be0e77ce18f5cb6f2c1556ae0
boldboxer <ebesteinpr@gmail.com>
2023-02-21 02:23:53 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,15 +9,13 @@
 void print_alphabet_x10(void)
 {
 	char c;
-	int j;
 
-	for (j <= 10)
+	int j <= 10;
+
+	for (c = 'a'; c <= 'z'; c++)
+
 	{
-
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-		}
-		_putchar('\n');
+		_putchar(c);
 	}
+	_putchar('\n');
 }

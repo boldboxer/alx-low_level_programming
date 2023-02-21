9ea87684bfb208fed131696d44817d077e38db81
boldboxer <ebesteinpr@gmail.com>
2023-02-21 02:14:03 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ *
+ * Retunr: (void)
+ */
+
+void print_alphabet_x10(void)
+{
+	char = c;
+	int j <= 10;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}

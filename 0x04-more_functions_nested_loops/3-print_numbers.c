5081456e99f17e494dd665fd5c627838c2908a9f
boldboxer <ebesteinpr@gmail.com>
2023-02-23 04:59:48 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers  0 to 9, followed by a new lin
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while(i <= 9)
+	{
+		_putchar('0' + i):
+			i++:
+	}_putchar('\n')
+}

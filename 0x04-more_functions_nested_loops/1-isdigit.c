45f2b340d49dec9363bfb476be4e89f22907fe8a
boldboxer <ebesteinpr@gmail.com>
2023-02-23 04:15:40 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+#include "main.h"
+/**
+ * _isdigit - checks wether a number passed as an argument is digit number
+ * @digit argument to be checked
+ * Return: returns 1 if successful or 0 if otherwise
+ */
+int _isdigit(int digit)
+{
+	if (digit >= '0' && digit <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

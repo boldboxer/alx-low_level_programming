944cdcd85f1154bafc9c3705cba64ac1f460a952
boldboxer <ebesteinpr@gmail.com>
2023-02-27 01:29:16 -0800

    first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * void reset_to_98 - takes a pointer to an int as parameter
+ *                    updates the value it points to to 98
+ *@n: random integer value
+ *Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

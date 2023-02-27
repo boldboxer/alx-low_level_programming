79d52c7b85982f39bf249baaad3db93493d264d7
boldboxer <ebesteinpr@gmail.com>
2023-02-27 02:15:52 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,12 +9,12 @@
 
 void _puts(char *str)
 {
-	int j = 0;
+	int i = 0;
 
-	while (str[j] != '0')
+	while (str[i] != '\0')
 	{
-		_putchar(str[j]);
-		j++;
+		_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
 }
